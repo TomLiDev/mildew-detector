3ab0f81753555237a5aa48a68f00ca7e35df6013
TomLiDev <t.litchfield00@gmail.com>
2024-05-16 17:18:36 +0000

    Customise dashboard icon

--- app_pages/multi_page.py
+++ app_pages/multi_page.py
@@ -12,7 +12,7 @@ class MultiPage:
 
         st.set_page_config(
             page_title = self.app_name,
-            page_icon="chart_with_upwards_trend",
+            page_icon= ":leaves:",
         )
 
 
